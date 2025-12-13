1d3ae7ef189cb8d1d695a420575ddd5938d37afd
Issa-barry <issabarry67@gmail.com>
2025-12-13 19:15:17 +0100

    add

--- src/app/pages/auth/auth.routes.ts
+++ src/app/pages/auth/auth.routes.ts
@@ -4,12 +4,16 @@ import { Access } from './access';
 import { Login } from './login/login';
 import { Register } from './register/register';
 import { Verification } from './verification/verification';
+import { Forgotpassword } from './forgotpassword/forgotpassword';
+import { Newpassword } from './newpassword/newpassword';
 
 export default [
     { path: 'access', component: Access },
     { path: 'error', component: Error },
     { path: 'login', component: Login },
     { path: 'register', component: Register},
-    { path:'verification', component:Verification}
+    { path:'register-verification', component:Verification},
+    { path:'forgot-password', component:Forgotpassword},
+    { path:'new-password', component:Newpassword}
 
 ] as Routes;
---
+++ src/app/pages/auth/newpassword/newpassword.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newpassword',
+  imports: [],
+  templateUrl: './newpassword.html',
+  styleUrl: './newpassword.scss'
+})
+export class Newpassword {
+
+}
